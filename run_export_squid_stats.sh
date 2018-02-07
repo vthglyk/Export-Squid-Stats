5fd52d0111025738e3fcc66d0e2c0ddab7c59498
vthglyk <vthglyk@gmail.com>
2018-02-07 09:31:33 +0000

    Changing directory to Export-Squid-Stats

--- run_export_squid_stats.sh
+++ run_export_squid_stats.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-screen -dm -S export-squid-stats python export_squid_stats.py --client_address=192.168.4.153
-#screen -dm -S export-squid-stats python export_squid_stats.py --client_address=192.168.4.153 --file tests/resources/test_access.log
+pushd /opt/squid/Export-Squid-Stats
+#screen -dm -S export-squid-stats python export_squid_stats.py --client_address=192.168.4.153
+screen -dm -S export-squid-stats python export_squid_stats.py --client_address=192.168.4.153 --file tests/resources/test_access.log
