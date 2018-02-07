4cb7567b11a5e5cc728df168998ae97489dfdfc9
vthglyk <vasgl@intracom-telecom.com>
2018-02-07 11:18:50 +0200

    Added test.log

--- run_export_squid_stats.sh
+++ run_export_squid_stats.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 screen -dm -S export-squid-stats python export_squid_stats.py --client_address=192.168.4.153
-#screen -dm -S export-squid-stats python export_squid_stats.py --client_address=192.168.4.153 --file test/resources/test_access.log
+#screen -dm -S export-squid-stats python export_squid_stats.py --client_address=192.168.4.153 --file tests/resources/test_access.log
